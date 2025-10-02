5acc35d5380dbcc5836e924d40820d9f81dc355b
Lubnafarheen <lubna.farheen2010@gmail.com>
2025-10-02 12:48:23 +0200

    first commit

---
+++ tests/JavascriptBasics/variables.js
@@ -0,0 +1,12 @@
+const pi = 3.14;
+let radius = 15;
+let areaOfCircle = pi*radius*radius;
+console.log(areaOfCircle);
+
+var a = 27;
+var b = 35;
+function sum(){
+    console.log(a*b);
+}
+
+sum();
